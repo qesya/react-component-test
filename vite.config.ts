76bad96ee013e58a113e12a6ea59aae5b3972dc2
Pasquale Palena <pasquale.palena@gmail.com>
2025-04-30 14:45:20 +0000

    fix: remove specific port

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,6 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    port: 2222, // Bisa sesuaikan port jika perlu
-  },
   test: {
     environment: "jsdom",
     globals: true,
